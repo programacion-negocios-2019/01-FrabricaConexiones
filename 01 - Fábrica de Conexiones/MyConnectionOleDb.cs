7bb409d71f3d945b6d5783acc67ec2774a01d3e2
Héctor Sabillón <h.hsabillon@gmail.com>
2019-05-27 12:54:46 -0600

    Creación de las clases MyConnectionSql y MyConnectionOleDb. Implementación de herencia y polimorfismo en el constructor.

---
+++ 01 - Fábrica de Conexiones/MyConnectionOleDb.cs
@@ -0,0 +1,6 @@
+﻿namespace _01___Fábrica_de_Conexiones
+{
+    internal class MyConnectionOleDb
+    {
+    }
+}
---
+++ 01 - Fábrica de Conexiones/MyConnectionSql.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Agregar los namespaces para la conexión con SQL Server
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _01___Fábrica_de_Conexiones
+{
+    class MyConnectionSql : MyConnection
+    {
+        public MyConnectionSql(DataProvider theDataProvider) : base(theDataProvider)
+        {
+            // Utilizando un ConnectionString para realizar la conexión
+            // con el servidor SQL Server
+            SqlConnection connectionString = new SqlConnection(@"server = (local)\sqlexpress;");
+        }
+    }
+}
