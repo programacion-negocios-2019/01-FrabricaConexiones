b6d3f1fb71c8156acaaf0960fe16b52b953e8496
Héctor Sabillón <h.hsabillon@gmail.com>
2019-05-29 12:17:39 -0600

    Finalizando la clase MyConnectionSql con los detalles de la conexión.

--- 01 - Fábrica de Conexiones/MyConnectionSql.cs
+++ 01 - Fábrica de Conexiones/MyConnectionSql.cs
@@ -12,7 +12,38 @@ namespace _01___Fábrica_de_Conexiones
         {
             // Utilizando un ConnectionString para realizar la conexión
             // con el servidor SQL Server
-            SqlConnection connectionString = new SqlConnection(@"server = (local)\sqlexpress;");
+            SqlConnection connectionString = new SqlConnection(@"server = (local)\sqlexpress; integrated security = true;");
+
+            try
+            {
+                // Abrir la conexión
+                connectionString.Open();
+
+                // Detalles de la conexión
+                this.DetallesConexion(connectionString);
+            }
+            catch (SqlException e)
+            {
+                // Desplegar el mensaje de error
+                Console.WriteLine("Error: {0} {1}", e.Message, e.StackTrace); ;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                connectionString.Close();
+                Console.WriteLine("Conexión finalizada");
+            }
+        }
+
+        protected void DetallesConexion(SqlConnection connectionString)
+        {
+            Console.WriteLine("Conexión establecida");
+            Console.WriteLine("\tConnection string: {0}", connectionString.ConnectionString);
+            Console.WriteLine("\tBase de datos: {0}", connectionString.Database);
+            Console.WriteLine("\tFuente de datos: {0}", connectionString.DataSource);
+            Console.WriteLine("\tVersión del servidor: {0}", connectionString.ServerVersion);
+            Console.WriteLine("\tEstado: {0}", connectionString.State);
+            Console.WriteLine("\tId estación de trabajo: {0}", connectionString.WorkstationId);
         }
     }
 }
